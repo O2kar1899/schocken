fb75427e6b2f8aceb389c65066ea1d46aeccc5fa
Heiner <git@pschp.de>
2022-02-26 11:20:13 +0100

    schocken.py round_one erter Wurf

--- schocken.py
+++ schocken.py
@@ -1,35 +1,41 @@
-import dice
+from curses import erasechar
+from dice import Dice
+import time
 
 
 class Schocken():
-    def number_of_players(self):
-        self.number_of_players = input(
-            "Bitte die Zahl der Spieler zwischen 1 und 3 eingeben: ")
-        if self.number_of_players > 0 and self.number_of_players <= 3:
-            start()
+    def __init__(self, number_of_points=13):
+        self.number_of_point = number_of_points
 
-    def start():
-        start = ""
-        while start == "":
-            start = input('"S" für Start oder "A" für Abbruch: ')
-            if start == "S" or "s":
-                print("Start", start)
-                return True
-            elif start == "A" or "a":
-                print("Das Spiel wurde abgebrochen!")
-                return False
-            else:
-                start = ""
+        # def number_of_players(self):
+        #     self.number_of_players = input(
+        #         "Bitte die Zahl der Spieler zwischen 1 und 3 eingeben: ")
+        #     if self.number_of_players > 0 and self.number_of_players <= 3:
+        #         start()
 
-    def round_one(self):
-        open_points = 13
+        # def start(self):
+        #     start = ""
+        #     while start == "":
+        #         start = input('"S" für Start oder "A" für Abbruch: ')
+        #         if start == "S" or "s":
+        #             print("Start", start)
+        #             return True
+        #         elif start == "A" or "a":
+        #             print("Das Spiel wurde abgebrochen!")
+        #             return False
+        #         else:
+        #             start = ""
 
-        while open_points > 0:
+    def round_one():
+        w = Dice(6)
+        result = w.roll_dices(3)
+        print("1, Würfel: ", result[0])
+        time.sleep(1)
+        print("2, Würfel: ", result[1])
+        time.sleep(2)
+        print("3, Würfel: ", result[2])
+        time.sleep(3)
 
 
-w = dice.Dice(6)
-
-
-print("1. Würfel: ", wurf[0])
-print("2. Würfel: ", wurf[1])
-print("3. Würfel: ", wurf[2])
+me = Schocken
+me.round_one()

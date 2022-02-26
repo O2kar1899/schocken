c4c89db808e960a3f9d9650b314cd32015ae930e
Heiner <git@pschp.de>
2022-02-26 10:43:18 +0100

    dice.py

--- dice.py
+++ dice.py
@@ -9,14 +9,14 @@ class Dice():
     #     value = randint(1, self.sides)
     #     return value
 
-    def random_side_grafic():
+    def random_side_grafic(self):
         pass
 
     def roll_dices(self, dices):
         self.dices = dices
-        print("Würfel", self.dices)
-        wurf = []
+        # print("Würfel", self.dices)
+        roll_dice_result = []
         for i in range(self.dices):
             value = randint(1, self.sides)
-            wurf.append(value)
-        return wurf
+            roll_dice_result.append(value)
+        return roll_dice_result

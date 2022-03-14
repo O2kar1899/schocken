f62f46750050e795b280f02c1bcbf3c400e5ea62
Heiner <git@pschp.de>
2022-03-14 11:20:06 +0100

    human_result

--- Schocken/human_play.py
+++ Schocken/human_play.py
@@ -1,3 +1,5 @@
+from operator import rshift
+from tkinter.messagebox import NO
 from dice import Dice
 from keep_or_not import *
 
@@ -11,22 +13,41 @@ class Play():
         self.result = []
         self.answers = [False, False, False]
 
-    def play(self):
+    def first_round(self):
         for roll in range(3):
             x = self.dice.roll_dice()
             self.result.append(x)
+        return self.result
 
         return self.result
 
-    def gg(self, result):
-        erg = keep_round_or_not(result)
+    def further_rounds(self, result):
+        kon = (keep_dice_or_not(result))
+        for i in range(len(kon)):
+            if kon[i] == False:
+                result[i] = human.dice.roll_dice()
+                print("Neu:", result[i])
+        self.round_number = 3
+        return result
+
+    def ergebnis():
+        pass
 
 
 human = Play('heiner')
-
-
-result = human.play()
+result = human.first_round()
 if keep_round_or_not(result) == True:
-    print('Wurf behalten')
+    rounds = 1
+    human_result = result
 else:
-    keep_dice_or_not(result)
+    rounds = 2
+    result = human.further_rounds(result)
+    human_result = None
+if human_result == None:
+    if keep_round_or_not(result) == True:
+        human_result = result
+    else:
+        rounds = 3
+        human_result = human.further_rounds(result)
+
+print(f'Dein Ergebnis: {human_result} im {rounds}. Versuch')
